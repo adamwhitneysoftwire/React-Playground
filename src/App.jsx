5b7ad816463870050b06b5f081ffe5aec9ef1911
adamwhitneysoftwire <adam.whitney@softwire.com>
2021-12-07 16:11:20 +0000

    Converted to functions and hooks instead of classes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
 import {PhotoViewer} from "./PhotoViewer/PhotoViewer";
+import React, {useState} from "react";
 
 function App() {
+    const PHOTO_NUM = 98;
+
+    const initialBw = Array(PHOTO_NUM);
+    initialBw.fill(false);
+
+    const [bw, setBw] = useState(initialBw);
+
+    const toggle = (index) => {
+        const newBw = [...bw];
+        for (let i = 0; i < bw.length; i++) {
+            if (i !== index) {
+                newBw[i] = false;
+            }
+        }
+        newBw[index] = !bw[index];
+        setBw(newBw);
+    }
+
+    const photos = [];
+    for (let i = 0; i < PHOTO_NUM; i++) {
+        photos.push(<PhotoViewer bw={bw[i]} index={i} toggle={toggle} />);
+    }
+
     return (
         <div>
-            <PhotoViewer />
+            <h1>React Photo Viewer</h1>
+            <div className="photo-container">
+                {photos}
+            </div>
         </div>
     );
 }
--- src/PhotoViewer/PhotoViewer.jsx
+++ src/PhotoViewer/PhotoViewer.jsx
@@ -1,29 +1,12 @@
 import React from "react";  // import React (to provide access to JSX)
 import "./PhotoViewer.css";
 
-// noinspection SpellCheckingInspection
-export class PhotoViewer extends React.Component {    // declare and export new function called 'PhotoViewer'
-    constructor(props) {
-        super(props);
-        this.state = {
-            bandw: props.bandw ? true : false
-        }
-
-        this.toggle = this.toggle.bind(this);
+export const PhotoViewer = ({bw, index, toggle}) => {
+    const toggleBw = () => {
+        toggle(index);
     }
 
-    render() {
-        return (
-            <div className="photo">
-                <img className={this.state.bandw ? "black-and-white" : ""} src="https://picsum.photos/1920/1080" onClick={this.toggle}/>
-                <h1>React Photo Viewer</h1>
-            </div>
-        );
-    }
-
-    toggle() {
-        this.setState(prevState => ({
-            bandw: !prevState.bandw
-        }));
-    }
+    return (
+        <img className={"photo" + (bw ? " black-and-white" : "")} src={"https://picsum.photos/id/" + index + "/128/128"} onClick={toggleBw} />
+    );
 }
